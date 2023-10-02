c7a756422934094036529b471928860f5dcea812
Hongdiyou <89882737+Hongdiyou@users.noreply.github.com>
2023-10-02 03:03:26 -0400

    sprint2#38_login_frontend
    src file

---
+++ src/Login.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import './Login.css'; // Import the CSS file for styling
+
+const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('Submitted:', { username, password });
+  };
+
+  return (
+    <div className="login-container">
+      <h2>Help me decide</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Username:</label>
+          <input type="text" value={username} onChange={handleUsernameChange} />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input type="password" value={password} onChange={handlePasswordChange} />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
