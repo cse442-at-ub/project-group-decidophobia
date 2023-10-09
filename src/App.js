202d2b0b858c0bd44af545d4a3dc72c10f5f2436
Hongdiyou <89882737+Hongdiyou@users.noreply.github.com>
2023-10-09 14:00:55 -0400

    Profile_frontend_sprint2#42

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ProfilePage from './ProfilePage';  // Update the path as needed
+
+const App = () => {
+  return (
+    <div className="App">
+      <ProfilePage />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/ProfilePage.js
@@ -0,0 +1,66 @@
+import React, { useState } from 'react';
+
+const ProfilePage = () => {
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleConfirmPasswordChange = (event) => {
+    setConfirmPassword(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // You can add validation logic here
+    console.log('Form submitted:', { username, email, password, confirmPassword });
+  };
+
+  return (
+    <div>
+      <h1>Profile Page</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Avatar:
+          {/* Add avatar component or input for avatar */}
+        </label>
+        <br />
+        <label>
+          Username:
+          <input type="text" value={username} onChange={handleUsernameChange} />
+        </label>
+        <br />
+        <label>
+          Email:
+          <input type="email" value={email} onChange={handleEmailChange} />
+        </label>
+        <br />
+        <label>
+          Password:
+          <input type="password" value={password} onChange={handlePasswordChange} />
+        </label>
+        <br />
+        <label>
+          Confirm Password:
+          <input type="password" value={confirmPassword} onChange={handleConfirmPasswordChange} />
+        </label>
+        <br />
+        <button type="submit">Save Changes</button>
+      </form>
+    </div>
+  );
+};
+
+export default ProfilePage;
