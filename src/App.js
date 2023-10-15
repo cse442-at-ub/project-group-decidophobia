6083c91d6b8ace4fb0393936975a55b5ed8c7fda
jhw02 <78567216+jhw02@users.noreply.github.com>
2023-10-15 16:01:40 -0400

    sprint2#44 posting page

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import './color _option.css';
+import QuestionForm from './question_form';
+
+function App() {
+  return (
+    <div className="App">
+      <QuestionForm />
+    </div>
+  );
+}
+
+export default App;
