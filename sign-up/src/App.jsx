3ac69a9c9f0bdf18e15e5bb6af25692dc8bbdd2a
Karl <33317161+SkyLanKarl@users.noreply.github.com>
2023-10-21 22:51:07 -0400

    sign up file
    the file of sign-up.

---
+++ sign-up/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from './Navbar';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar /> {/* 添加Navbar组件 */}
+      {/* 其他页面内容 */}
+    </div>
+  );
+}
+
+export default App;
